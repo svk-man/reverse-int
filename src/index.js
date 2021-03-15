4c7a5217cbdc86c04f4dbe1650263153e5e81e9c
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-03-15 20:59:41 +0300

    refactor: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let str = String(n);
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] != '-') {
+            result += str[i];
+        }
+    }
+
+    return result;
 }
